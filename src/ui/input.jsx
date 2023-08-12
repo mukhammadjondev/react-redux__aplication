be31b29d53804a254b5a1522874b02d99bd17a31
mukhammadjondev <muhsdev@gmail.com>
2023-08-12 22:44:00 +0500

    validation form

--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,8 +1,25 @@
-const Input = ({label, state, setState, type = 'text', id}) => {
+import { useCallback } from "react"
+import { useSelector } from "react-redux"
+
+const Input = ({label, state, setState, type = 'text'}) => {
+  const {error} = useSelector(state => state.auth)
+
+  const errorMessage = useCallback(() => {
+    return Object.keys(error).map(name => {
+      const msg = error[name].join(', ')
+      if(label.toLowerCase() == name) {
+        return <div key={name} className="invalid-feedback d-block">{msg}</div>
+      } else if ('emailOrPassword' == name) {
+        return <div key={name} className="invalid-feedback d-block">{name} {msg}</div>
+      }
+    })
+  }, [error])
+
   return (
     <div className='form-floating mt-2'>
-			<input type={type} className='form-control' value={state} onChange={e => setState(e.target.value)}id={id} placeholder={label} />
-			<label htmlFor={id}>{label}</label>
+			<input type={type} className='form-control' value={state} onChange={e => setState(e.target.value)}id={label} placeholder={label} />
+			<label htmlFor={label}>{label}</label>
+      {error !== null && errorMessage()}
 		</div>
   )
 }
