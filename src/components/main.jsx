6f5e60aca3444f67757c701cc317d11e8b1cef42
mukhammadjondev <muhsdev@gmail.com>
2023-08-13 15:31:27 +0500

    article cards and loader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,36 @@
+import { useSelector } from "react-redux"
+import { Loader } from '../ui'
+
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+  const {articles, isLoading} = useSelector(state => state.article)
+
+  return <div className="container">
+    {isLoading && <Loader />}
+    <div className="album py-5">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {articles.map(item => (
+        <div className="col" key={item.id}>
+          <div className="card h-100 shadow-sm">
+            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
+
+            <div className="card-body">
+              <p className="card-text fw-bold m-0">{item.title}</p>
+              <p className="card-text">{item.description}</p>
+            </div>
+            <div className="card-footer d-flex justify-content-between align-items-center">
+              <div className="btn-group">
+                <button type="button" className="btn btn-sm btn-outline-success">View</button>
+                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
+              </div>
+              <small className="text-muted fw-bold text-capitalize">{item.author.username}</small>
+            </div>
+          </div>
+        </div>
+        ))}
+      </div>
+    </div>
+  </div>
 }
 
 export default Main
---
+++ src/ui/loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="d-flex mx-auto align-items-center" style={{height: '75vh'}}>
+      <div class="spinner-border d-flex mx-auto align-items-center" style={{width: '4rem', height: '4rem'}} role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
