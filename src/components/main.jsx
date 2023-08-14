a6f1a451ffaece081ac60066faef486b1c0a901d
mukhammadjondev <muhsdev@gmail.com>
2023-08-14 11:45:30 +0500

    article delete and edit & optimization

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
 import ArticleService from "../service/article"
 import { getArticlesStart, getArticlesSuccess } from "../slice/article"
 import { Loader } from '../ui'
+import ArticleCard from "./article-card"
 
 const Main = () => {
   const {articles, isLoading} = useSelector(state => state.article)
-  const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const getArticles = async () => {
@@ -29,24 +28,7 @@ const Main = () => {
     <div className="album py-5">
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {articles.map(item => (
-        <div className="col" key={item.id}>
-          <div className="card h-100 shadow-sm">
-            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
-
-            <div className="card-body">
-              <p className="card-text fw-bold m-0">{item.title}</p>
-              <p className="card-text">{item.description}</p>
-            </div>
-            <div className="card-footer d-flex justify-content-between align-items-center">
-              <div className="btn-group">
-                <button onClick={() => navigate(`/article/${item.slug}`)} type="button" className="btn btn-sm btn-outline-success">View</button>
-                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
-              </div>
-              <small className="text-muted fw-bold text-capitalize">{item.author.username}</small>
-            </div>
-          </div>
-        </div>
+          <ArticleCard item={item} getArticles={getArticles} />
         ))}
       </div>
     </div>
